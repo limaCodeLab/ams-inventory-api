b61aa6618f87099855465882de3209190232b127
limaCodeLab <alan.byte.prog@outlook.com>
2024-06-10 09:23:37 -0300

    Inventory service config

---
+++ backend/ats-inventory-mgmt-api/src/main/java/com/skald/ats/inventory/api/AtsInventoryMgmtApiApplication.java
@@ -0,0 +1,13 @@
+package com.skald.ats.inventory.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsInventoryMgmtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsInventoryMgmtApiApplication.class, args);
+	}
+
+}
