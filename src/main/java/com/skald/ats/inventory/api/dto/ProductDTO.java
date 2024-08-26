877380ba0b586c36a1aff28f8f68ceb8b1ecffa2
limaCodeLab <alan.byte.prog@outlook.com>
2024-08-26 17:37:35 -0300

    Update lombok annotations

--- src/main/java/com/skald/ats/inventory/api/dto/ProductDTO.java
+++ src/main/java/com/skald/ats/inventory/api/dto/ProductDTO.java
@@ -4,13 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
+import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Getter
+@Data
 public class ProductDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
--- src/main/java/com/skald/ats/inventory/api/model/Product.java
+++ src/main/java/com/skald/ats/inventory/api/model/Product.java
@@ -3,17 +3,18 @@ package com.skald.ats.inventory.api.model;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 @Table(name = "tb_product")
 public class Product implements Serializable {
     @Serial
@@ -52,33 +53,10 @@ public class Product implements Serializable {
     @Column(updatable = false)
     private LocalDateTime dateCreated;
 
-    public Product(){}
-
     @PrePersist
     @PreUpdate
     private void trimFields(){
         this.name = (name != null) ? name.trim() : null;
         this.description = (description != null) ? description.trim() : null;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Product product)) return false;
-        return Objects.equals(id, product.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Product [id=" + id + ", name=" + name + ", description=" + description + ", supplier=" + supplier
-                + ", category=" + category + ", costPrice=" + unitPrice + ", minimalStockLevel=" + minimalStockLevel
-                + ", maximumStockLevel=" + maximumStockLevel + ", dateCreated=" + dateCreated
-                + ", status=" + status + ", quantityOnHand=" + quantityOnHand + "]";
-    }
-
 }
